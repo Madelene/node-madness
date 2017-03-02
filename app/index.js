760ff676e7b481d6f74478da2ec0e965ee856385
Madelene Campos <madelenecampos@gmail.com>
2017-03-02 17:05:22 -0500

    Organizes index file into the given examples

--- app/index.js
+++ app/index.js
@@ -1,8 +1,4 @@
-const calc = require('./calc')
-const _    = require('lodash')
-
-_.assign({'a': 1}, {'b': 2}, {'c': 3});
-
+const calc    = require('./calc')
 const numbersToAdd = [
   3,
   4,
@@ -12,3 +8,19 @@ const numbersToAdd = [
 
 const result = calc.sum(numbersToAdd)
 console.log(`The result is: ${result}`)
+
+// #########################################
+
+const _       = require('lodash')
+_.assign({'a': 1}, {'b': 2}, {'c': 3});
+
+// #########################################
+
+const numbers = [2,4,1,5,4]
+
+function isBiggerThanTwo (num) {
+  return num > 2
+}
+
+numbers.filter(isBiggerThanTwo)
+

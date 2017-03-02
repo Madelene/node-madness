1ade128b6d849f27d47ef1ea21a93c2b6bff63f6
Madelene Campos <madelenecampos@gmail.com>
2017-03-02 18:08:07 -0500

    Defines middlewares and adds error handling

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ require('./app/index')
 
 const express = require('express')
 const app = express()
-const port = 3000
+
+app.use((request, response, next) => {
+  console.log(request.headers)
+  next()
+})
+
+app.use((request, response, next) => {
+  request.chance = Math.random()
+  next()
+})
 
 app.get('/', (request, response) => {
-  response.send("Maddie's first Express server!")
+  response.json({
+    chance: request.chance
+  })
 })
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
+app.get('/', (request, response) => {
+  throw new Error('oops')
 })
+
+app.use((err, request, response, next) => {
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+app.listen(3000)
